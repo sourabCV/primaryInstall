0d0675f74e9f185de2aa2b3cfeb18da86b70c091
Sourab Maity <144662563+sourabCV@users.noreply.github.com>
2024-07-03 22:22:21 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# opencv install
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev 
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python3-pip
 pip3 install numpy
-pip3 install pandas
+apt list python*opencv*
+sudo apt install python3-opencv 
+apt show python3-opencv
+
+# vidgear install
+pip3 install -U vidgear[asyncio]
+
+
