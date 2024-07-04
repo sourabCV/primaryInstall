bb246fb06d019de482d691a6c432c8287daf6e52
Sourab Maity <144662563+sourabCV@users.noreply.github.com>
2024-07-04 15:35:22 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -2,13 +2,14 @@
 
 # opencv install
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install libopenblas-dev -y
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
 sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt-get install libgtk2.0-dev -y
 sudo apt-get install libatlas-base-dev gfortran -y
 sudo apt-get install python3-pip -y
-pip3 install numpy==1.26.4 -y
+pip3 install numpy==1.26.4
 apt list python*opencv*
 sudo apt install python3-opencv -y
 apt show python3-opencv
@@ -16,6 +17,8 @@ apt show python3-opencv
 # vidgear install
 #pip3 install -U vidgear[asyncio]
 pip3 install --no-deps vidgear
-
+pip3 install colorlog
+pip3 install tqdm
+pip3 install pyzmq
 
 
