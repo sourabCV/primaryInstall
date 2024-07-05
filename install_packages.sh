06918b2baf3bc1e621b20a0b1fdd065f082c8bcf
Sourab Maity <144662563+sourabCV@users.noreply.github.com>
2024-07-05 14:15:47 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -13,6 +13,7 @@ pip3 install numpy==1.26.4
 apt list python*opencv*
 sudo apt install python3-opencv -y
 apt show python3-opencv
+echo "CV2 installation complete."
 
 # vidgear install
 #pip3 install -U vidgear[asyncio]
@@ -20,5 +21,20 @@ pip3 install --no-deps vidgear
 pip3 install colorlog
 pip3 install tqdm
 pip3 install pyzmq
+echo "Vidgear installation complete."
 
+# anydesk install
+LATEST_URL=$(wget -qO- https://anydesk.com/en/downloads/Raspberry%20Pi | grep -oP '(?<=href=")[^"]+anydesk_[^"]+armhf.deb' | head -n 1)
+LATEST_URL="https://download.anydesk.com${LATEST_URL}"
+wget $LATEST_URL -O anydesk_latest_armhf.deb
+sudo dpkg -i anydesk_latest_armhf.deb
+sudo apt --fix-broken install -y
+rm anydesk_latest_armhf.deb
+echo "Anydesk installation complete."
 
+# Teamviewer install
+wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer_latest_armhf.deb
+sudo dpkg -i teamviewer_latest_armhf.deb
+sudo apt --fix-broken install -y
+rm teamviewer_latest_armhf.deb
+echo "TeamViewer installation complete."
