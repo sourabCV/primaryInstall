2a32a6f4246d01d52ba62479f291874c13c4cc2b
Sourab Maity <144662563+sourabCV@users.noreply.github.com>
2024-07-05 16:22:28 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 sudo apt update && sudo apt upgrade -y
 ANYDESK_URL="https://download.anydesk.com/rpi/anydesk_6.2.1-1_armhf.deb"
 wget $ANYDESK_URL -O anydesk_latest_armhf.deb
+if [ $? -ne 0 ]; then
+  echo "Error downloading AnyDesk. Please check the URL and try again."
+  exit 1
+fi
 sudo dpkg -i anydesk_latest_armhf.deb
 sudo apt --fix-broken install -y
 rm anydesk_latest_armhf.deb
